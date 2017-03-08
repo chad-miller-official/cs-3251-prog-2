e8e32886c812da76fbb35db5fef54673247a217a
Chad Miller <chad.miller.3333@gmail.com>
2017-03-07 23:42:49 -0500

    Removed unnecessary fields

--- reldat.py
+++ reldat.py
@@ -1,27 +1,8 @@
 from subprocess import check_output
 
 class Reldat( object ):
-    # if dest_ip is none, then we are the server
-    def __init__( self, port, src_max_window_size, dest_ip=None ):
-        self.dst_ip              = dest_ip
-        self.dst_max_window_size = None
-
-        self.src_ip              = check_output( [ 'hostname', '-I' ] ).split()[0]
-        self.src_max_window_size = src_max_window_size
-
-        self.port = port
-
-    def get_dst_ip_address( self ):
-        return self.dst_ip
-
-    def get_src_ip_address( self ):
-        return self.src_ip
-
-    def get_port( self ):
-        return self.port
-
-    def get_dst_max_window_size( self ):
-        return self.dst_max_window_size
+    def __init__( self, max_window_size ):
+        self.src_max_window_size = max_window_size
 
     def get_src_max_window_size( self ):
         return self.src_max_window_size
