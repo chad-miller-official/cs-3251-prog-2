80fcaab70ead38f2b1dd16b08feb00c60d269db8
Chad Miller <chad.miller.3333@gmail.com>
2017-03-09 12:23:46 -0500

    Added packetizer

--- reldat.py
+++ reldat.py
@@ -1,12 +1,13 @@
 import hashlib
+import math
 import pickle
 import socket
 import struct
 
-from subprocess import check_output
-
 '''
-Packet structure:
+The next four functions construct and deconstruct packets.
+Packet structure is as follows:
+
 0000[R][A][C][O]               1 byte
 [Sequence Number]              4 bytes
 [ACK Number]                   4 bytes
@@ -20,6 +21,95 @@ R = Retransmission bit (1 if data in payload has already been transmitted
 A = ACK bit (1 if packet is an acknowledgement; false otherwise)
 C = Request for connection close bit (only 1 during connection open process)
 O = Request for connection open bit (only 1 during connection close process)
+'''
+
+_ULL = 0xFFFFFFFFFFFFFFFF
+
+def _construct_header( data, seq_num, ack_num, flags=[] ):
+    header_flags = 0
+
+    for flag in flags:
+        header_flags |= flag
+
+    hash_calc = hashlib.md5()
+    hash_calc.update( data )
+    checksum = int( hash_calc.hexdigest(), 16 )
+
+    checksum_parts = (
+        ( checksum >> 64 ) & _ULL,
+        checksum & _ULL
+    )
+
+    return struct.pack(
+        '!'   # use network order
+        'B'   # flags
+        'I'   # seq num
+        'I'   # ack num
+        '2Q', # checksum
+        header_flags, seq_num, ack_num, checksum_parts[0], checksum_parts[1]
+    )
+
+def _construct_packet( data, seq_num, ack_num, flags=[] ):
+    header = _construct_header( data, seq_num, ack_num, flags )
+
+    hash_calc = hashlib.md5()
+    hash_calc.update( header )
+
+    header_checksum       = int( hash_calc.hexdigest(), 16 )
+    header_checksum_parts = (
+        ( header_checksum >> 64 ) & _ULL,
+        header_checksum & _ULL
+    )
+
+    header_checksum_struct = struct.pack(
+        '!'   # use network order
+        '2Q', # checksum
+        header_checksum_parts[0], header_checksum_parts[1]
+    )
+
+    return header + header_checksum_struct + data
+
+def _deconstruct_header( packet_header ):
+    ( flags, seq_num, ack_num, checksum_1, checksum_2 ) = struct.unpack(
+        '!BII2Q',
+        packet_header
+    )
+
+    checksum = ( ( checksum_1 & _ULL ) << 64 ) | ( checksum_2 & _ULL )
+    return ( flags, seq_num, ack_num, checksum )
+
+def _deconstruct_packet( packet_data ):
+    packet_header   = packet_data[:PACKET_HEADER_SIZE - 16]
+    header_checksum = packet_data[PACKET_HEADER_SIZE - 16 : PACKET_HEADER_SIZE]
+    packet_payload  = packet_data[PACKET_HEADER_SIZE:]
+
+    ( header_checksum_1, header_checksum_2 ) = struct.unpack(
+        '!2Q',
+        header_checksum
+    )
+
+    header_checksum = ( ( header_checksum_1 & _ULL ) << 64 ) | ( header_checksum_2 & _ULL )
+
+    hash_calc = hashlib.md5()
+    hash_calc.update( packet_header )
+    expected_header_checksum = hash_calc.hexdigest()
+
+    if header_checksum != int( expected_header_checksum, 16 ):
+        raise HeaderCorruptedError()
+
+    ( flags, seq_num, ack_num, checksum ) = _deconstruct_header( packet_header )
+
+    hash_calc = hashlib.md5()
+    hash_calc.update( packet_payload )
+    expected_checksum = hash_calc.hexdigest()
+
+    if checksum != int( expected_checksum, 16 ):
+        raise PayloadCorruptedError()
+
+    return ( flags, seq_num, ack_num, packet_payload )
+
+'''
+The following class defines a RELDAT communication channel object.
 '''
 
 # All sizes are in bytes
@@ -38,9 +128,6 @@ RESERVE_FLAG_3  = 0b01000000
 RESERVE_FLAG_4  = 0b10000000
 
 class Reldat( object ):
-    # Constants
-    __ULL = 0xFFFFFFFFFFFFFFFF
-
     def __init__( self, max_window_size ):
         self.src_ip_address      = socket.gethostbyname( socket.gethostname() )
         self.src_max_window_size = max_window_size
@@ -51,88 +138,6 @@ class Reldat( object ):
         self.port       = None
         self.in_socket  = None
         self.out_socket = None
-
-    def __construct_header( self, data, seq_num, ack_num, flags=[] ):
-        header_flags = 0
-
-        for flag in flags:
-            header_flags |= flag
-
-        hash_calc = hashlib.md5()
-        hash_calc.update( data )
-        checksum = int( hash_calc.hexdigest(), 16 )
-
-        checksum_parts = (
-            ( checksum >> 64 ) & self.__ULL,
-            checksum & self.__ULL
-        )
-
-        return struct.pack(
-            '!'   # use network order
-            'B'   # flags
-            'I'   # seq num
-            'I'   # ack num
-            '2Q', # checksum
-            header_flags, seq_num, ack_num, checksum_parts[0], checksum_parts[1]
-        )
-
-    def __construct_packet( self, data, seq_num, ack_num, flags=[] ):
-        header = self.__construct_header( data, seq_num, ack_num, flags )
-
-        hash_calc = hashlib.md5()
-        hash_calc.update( header )
-
-        header_checksum       = int( hash_calc.hexdigest(), 16 )
-        header_checksum_parts = (
-            ( header_checksum >> 64 ) & self.__ULL,
-            header_checksum & self.__ULL
-        )
-
-        header_checksum_struct = struct.pack(
-            '!2Q',
-            header_checksum_parts[0], header_checksum_parts[1]
-        )
-
-        return header + header_checksum_struct + data
-
-    def __deconstruct_header( self, packet_header ):
-        ( flags, seq_num, ack_num, checksum_1, checksum_2 ) = struct.unpack(
-            '!B2I2Q',
-            packet_header
-        )
-
-        checksum = ( ( checksum_1 & self.__ULL ) << 64 ) | ( checksum_2 & self.__ULL )
-        return ( flags, seq_num, ack_num, checksum )
-
-    def __deconstruct_packet( self, packet_data ):
-        packet_header   = packet_data[:PACKET_HEADER_SIZE - 16]
-        header_checksum = packet_data[PACKET_HEADER_SIZE - 16 : PACKET_HEADER_SIZE]
-        packet_payload  = packet_data[PACKET_HEADER_SIZE:]
-
-        ( header_checksum_1, header_checksum_2 ) = struct.unpack(
-            '!2Q',
-            header_checksum
-        )
-
-        header_checksum = ( ( header_checksum_1 & self.__ULL ) << 64 ) | ( header_checksum_2 & self.__ULL )
-
-        hash_calc = hashlib.md5()
-        hash_calc.update( packet_header )
-        expected_header_checksum = hash_calc.hexdigest()
-
-        if header_checksum != int( expected_header_checksum, 16 ):
-            raise HeaderCorruptedError()
-
-        ( flags, seq_num, ack_num, checksum ) = self.__deconstruct_header( packet_header )
-
-        hash_calc = hashlib.md5()
-        hash_calc.update( packet_payload )
-        expected_checksum = hash_calc.hexdigest()
-
-        if checksum != int( expected_checksum, 16 ):
-            raise PayloadCorruptedError()
-
-        return ( flags, seq_num, ack_num, packet_payload )
 
     def connect( self, dst_ip_address, port ):
         self.dst_ip_address = dst_ip_address
@@ -146,7 +151,9 @@ class Reldat( object ):
         payload_encoded = pickle.dumps( payload )
 
         # Construct the initial request for open packet
-        packet = self.__construct_packet( payload_encoded, 0, 0, [ OPEN_FLAG ] )
+        # packet = _construct_packet( payload_encoded, 0, 0, [ OPEN_FLAG ] )
+
+        self.send( 'This is a test of a string' )
 
         # TODO
 
@@ -159,9 +166,53 @@ class Reldat( object ):
 
         # TODO
 
+    def send( self, data ):
+        packetizer = PacketIterator( data, self.dst_max_window_size )
+
+        for packet in packetizer:
+            # TODO
+            pass
+
+    def recv( self, data ):
+        # TODO
+        pass
+
     def disconnect( self ):
         # TODO
         pass
+
+'''
+The following class defines a black box that accepts data and packetizes
+it. Every iteration returns the next packet that composes the data.
+'''
+
+class PacketIterator:
+    def __init__( self, data, window_size ):
+        self.data        = data
+        self.window_size = window_size
+
+        self.last_packet_num = int( math.ceil( len( data ) / float( PACKET_PAYLOAD_SIZE ) ) )
+        self.curr_packet_num = 0
+
+    def __iter__( self ):
+        return self
+
+    def next( self ):
+        if self.curr_packet_num >= self.last_packet_num:
+            raise StopIteration
+        else:
+            send_data_start = self.curr_packet_num * PACKET_PAYLOAD_SIZE
+            send_data_end   = ( self.curr_packet_num + 1 ) * PACKET_PAYLOAD_SIZE
+
+            if send_data_end > len( self.data ):
+                send_data = self.data[send_data_start:]
+            else:
+                send_data = self.data[send_data_start : send_data_end]
+
+            self.curr_packet_num += 1
+            packet                = _construct_packet( send_data, self.curr_packet_num, 0 )
+
+            return packet
 
 '''
 The following classes define exceptions that may be raised during transmission.
