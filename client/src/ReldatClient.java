55d58026a20baedacc9534399c4ffbc38b259d57
rbradshaw6 <rbradshaw6@gatech.edu>
2017-03-14 11:34:01 -0400

    I/O in reldat client added

--- client/src/ReldatClient.java
+++ client/src/ReldatClient.java
@@ -4,25 +4,30 @@ import java.util.regex.Pattern;
 
 import reldat.ReldatConnection;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReldatClient
 {
 	public static void main( String[] args )
 	{
 		if( args.length != 2 )
 			usage();
-		
+
 		Pattern hostRegex = Pattern.compile( "^(\\d{1,3}[.]\\d{1,3}[.]\\d{1,3}[.]\\d{1,3}):(\\d{1,5})$" );
 		Matcher hostMatch = hostRegex.matcher( args[0] );
-		
+
 		if( !hostMatch.matches() )
 			usage();
 
 		String ipAddress = hostMatch.group( 1 );
 		int port         = Integer.parseInt( hostMatch.group( 2 ) );
-		
+
 		if( port > 65535 )
 			usage();
-		
+
 		int maxReceiveWindowSize = Integer.parseInt( args[1] );
 
 		ReldatConnection reldatConn = new ReldatConnection( maxReceiveWindowSize );
@@ -36,25 +41,25 @@ public class ReldatClient
 		System.out.println( "Usage: java ReldatClient <host IP address>:<host port> <max receive window size in packets>" );
 		System.exit( 0 );
 	}
-	
+
 	public static void commandLoop( ReldatConnection reldatConn )
 	{
 		boolean disconnect = false;
 		Scanner scanner    = new Scanner( System.in );
 		scanner.useDelimiter( "\n" );
-		
+
 		while( !disconnect )
 		{
 			System.out.print( "> " );
 			String clientInput = scanner.next();
-			
+
 			Pattern commandRegex = Pattern.compile( "(\\w+)\\s*(.+)?" );
 			Matcher commandMatch = commandRegex.matcher( clientInput );
-			
+
 			if( commandMatch.matches() )
 			{
 				String command = commandMatch.group( 1 );
-				
+
 				switch( command )
 				{
 					case "disconnect":
@@ -62,12 +67,11 @@ public class ReldatClient
 						break;
 					case "transform":
 						String filename = commandMatch.group( 2 );
-						
+
 						if( filename == null || filename.isEmpty() )
 							System.out.println( "  Usage: transform <file>" );
 						else
 							transform( reldatConn, filename );
-						
 						break;
 					default:
 						System.out.println( "Unrecognized command. Valid commands are:\n    disconnect\n    transform" );
@@ -75,12 +79,25 @@ public class ReldatClient
 				}
 			}
 		}
-		
+
 		scanner.close();
 	}
-	
-	public static void transform( ReldatConnection reldatConn, String filename )
-	{
-		// TODO
+
+	public static void transform( ReldatConnection reldatConn, String filename ) {
+        try {
+			FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+
+			String sCurrentLine = null;
+            String newStr = "";
+
+			while ((sCurrentLine = br.readLine()) != null) {
+                newStr += sCurrentLine.toUpperCase();
+			}
+
+            System.out.println(newStr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
